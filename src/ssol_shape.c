32e47a1dec694729df6a2d032840b3be9f9e9416
Vincent Forest <vincent.forest@meso-star.com>
2017-01-13 14:16:09 +0100

    Fix quadric intersection
    The quadric transformation was wrong on ray intersection.

--- src/ssol_shape.c
+++ src/ssol_shape.c
@@ -802,9 +802,9 @@ punched_shape_project_point
   ASSERT(shape->type == SHAPE_PUNCHED);
 
   /* Compute world<->quadric space transformations */
-  d33_muld33(R, shape->quadric.transform, transform);
-  d33_muld3 (T, shape->quadric.transform, transform+9);
-  d3_add(T, shape->quadric.transform + 9, T);
+  d33_muld33(R, transform, shape->quadric.transform);
+  d33_muld3(T, transform, shape->quadric.transform+9);
+  d3_add(T, T, transform + 9);
   d33_invtrans(R_invtrans, R);
   d3_minus(T_inv, T);
 
@@ -848,9 +848,9 @@ punched_shape_trace_ray
   ASSERT(shape->type == SHAPE_PUNCHED);
 
   /* Compute world<->quadric space transformations */
-  d33_muld33(R, shape->quadric.transform, transform);
-  d33_muld3 (T, shape->quadric.transform, transform+9);
-  d3_add(T, shape->quadric.transform + 9, T);
+  d33_muld33(R, transform, shape->quadric.transform);
+  d33_muld3(T, transform, shape->quadric.transform+9);
+  d3_add(T, T, transform + 9);
   d33_invtrans(R_invtrans, R);
   d3_minus(T_inv, T);
 
