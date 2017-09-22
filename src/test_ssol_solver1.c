8850e89f83292a0543db339b8b923c77677422ec
Christophe Coustet <christophe.coustet@meso-star.com>
2017-09-22 16:16:59 +0200

    Fix line unexpectedly modified in a previous commit.
    Test1 was single threaded, now multithreaded again.

--- src/test_ssol_solver1.c
+++ src/test_ssol_solver1.c
@@ -104,7 +104,7 @@ main(int argc, char** argv)
   mem_init_proxy_allocator(&allocator, &mem_default_allocator);
 
   CHECK(ssol_device_create
-    (NULL, &allocator, (SSOL_NTHREADS_DEFAULT,1), 0, &dev), RES_OK);
+    (NULL, &allocator, SSOL_NTHREADS_DEFAULT, 0, &dev), RES_OK);
 
   CHECK(ssp_rng_create(&allocator, &ssp_rng_threefry, &rng), RES_OK);
 
